88f13cba74a24c3ac671f649cdfa69c4e4831e85
LPMartinez <lpmartinez1109@gmail.com>
2020-11-03 11:20:47 -0600

    pages folder created for use:

---
+++ grace_photography/src/pages/Home/home.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+
+export default class Home extends Component{
+    render(){
+        return(
+                <header id="home">
+                    <nav id="nav-bar">
+                        <a className="mobile-btn" href="#nav-bar" title="Show navigation">Show navigation</a>
+                    </nav>
+                </header>
+
+        )
+    }
+}
