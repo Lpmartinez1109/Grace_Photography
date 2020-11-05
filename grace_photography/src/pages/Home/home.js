62391d1266cb6a9017e26a3a6301abb10a953e0d
LPMartinez <lpmartinez1109@gmail.com>
2020-11-04 19:31:13 -0600

    nav bar start on site

--- grace_photography/src/pages/Home/home.js
+++ grace_photography/src/pages/Home/home.js
@@ -6,6 +6,10 @@ export default class Home extends Component{
                 <header id="home">
                     <nav id="nav-bar">
                         <a className="mobile-btn" href="#nav-bar" title="Show navigation">Show navigation</a>
+                        <a className="mobile-btn" href="#" title="Hide navigation">Hide navigation</a>
+                        <ul id="nav" className="nav">
+                            <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
+                        </ul>
                     </nav>
                 </header>
 
