c86cd7125f687986f2e8683658d9e80c6d93c79d
LPMartinez <lpmartinez1109@gmail.com>
2020-12-03 16:17:19 -0600

    Building routes for different pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import Home from './pages/Home/home';
+import { BroswerRouter as Router, Route, Switch } from "react-router-dom";
 
 class App extends Component{
   render(){
     return(
+      <Router>
       <div className="App">
         <Home />
       </div>
+      </Router>
     )
   }
 };
