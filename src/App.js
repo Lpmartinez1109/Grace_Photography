077f4a05aa96f3506e92629128224ac92ba35614
LPMartinez <lpmartinez1109@gmail.com>
2020-12-04 08:35:19 -0600

    More routes added to file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import Home from './pages/Home/home';
-import { BroswerRouter as Router, Route, Switch } from "react-router-dom";
+import Works from './pages/Works/works';
+import Contact from './pages/Contact/contact';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 class App extends Component{
   render(){
     return(
       <Router>
       <div className="App">
-        <Home />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/works" component={ Works } />
+          <Route exact path="/contact" component={ Contact } />
+        </Switch>
       </div>
       </Router>
     )
