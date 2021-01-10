b847611c805b564076abd7d0696c9f4635c0186d
LPMartinez <lpmartinez1109@gmail.com>
2021-01-09 21:09:27 -0600

    fixed contact navbar to display current page

--- src/pages/Contact/contact.js
+++ src/pages/Contact/contact.js
@@ -6,13 +6,15 @@ export default class Contact extends Component{
         return(
             <React.Fragment>
                 <section id="contact">
-                <nav id="nav-bar">
+                <nav id="nav-wrap">
                         <a className="mobile-btn" href="#nav-bar" title="Show navigation">Show navigation</a>
                         <a className="mobile-btn" href="#" title="Hide navigation">Hide navigation</a>
                         <ul id="nav" className="nav">
-                            <li className="current"><a className="smoothscroll" href="/">Home</a></li>
+                            <li><a className="smoothscroll" href="/">Home</a></li>
+                            <li><a className="smoothscroll" href="/about">About</a></li>
                             <li><a className="smoothscroll" href="/works">Works</a></li>
-                            <li><a className="smoothscroll" href="/contact">Contact Me</a></li>
+                            <li className="current"
+                            ><a className="smoothscroll" href="/contact">Contact Me</a></li>
                         </ul>
                     </nav>
                     <div className="row section-head">
